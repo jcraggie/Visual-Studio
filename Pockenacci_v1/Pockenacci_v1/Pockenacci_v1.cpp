acb948d3b0f2b100c9ce48c159fa5a9720aab5f3
Jason C. Rogers <jcraggie@hotmail.com>
2016-09-24 07:06:27 -0500

    trying to implement classes for the key. unable to translate the needed 521436 back to the original key SECRET.

--- Pockenacci_v1/Pockenacci_v1/Pockenacci_v1.cpp
+++ Pockenacci_v1/Pockenacci_v1/Pockenacci_v1.cpp
@@ -26,35 +26,158 @@
 
 using namespace std;
 
+class KeyChar
+{
+public:
+	KeyChar(const char& c = ' ', const int& pos = 0, const int& sort_pos = 0, const int& value = 0);
+	char c;
+	int  pos;
+	int  sort_pos;
+	int  value;
+
+	KeyChar* GetNext() const;
+	void SetNext(KeyChar* next);
+
+private:
+	KeyChar* m_pNext;
+};
+
+KeyChar::KeyChar(const char& c, const int& pos, const int& sort_pos, const int& value )
+{
+	// constructor
+}
+
+KeyChar* KeyChar::GetNext() const
+{
+	return m_pNext;
+}
+
+void KeyChar::SetNext(KeyChar* next)
+{
+	m_pNext = next;
+}
+
+class Key
+{
+public:
+	Key();
+	~Key();
+	void ClearKey();
+	void DelFirstKey();
+	void AddKeyChar(string keyString);
+
+private:
+	KeyChar* m_pHead;
+	KeyChar* m_pLast;
+};
+
+// key constructor
+Key::Key() :
+	m_pHead(0),
+	m_pLast(0)
+{
+
+}
+
+// key destructor
+Key::~Key()
+{
+	ClearKey();
+}
+
+void Key::AddKeyChar(string keyString)
+{
+	// need to parse out keyString and get all it's atributes
+
+	string key = keyString;
+	vector<char> vKeyCharOriginal(key.begin(), key.end());
+	vector<char> vSortedKey = vKeyCharOriginal;
+	vector<int>vKeyInt;
+	vector<char>::const_iterator iterC;
+	vector<int>::const_iterator iterI;
+	vector<char>::const_iterator iterO;
+	int keySize = key.size();
+	int pos = 0;
+	char c;
+	int sorted_pos;
+	int value;
+
+	cout << key << " is the original key string keyString. " << keySize << " is the size of keyString." << endl;
+
+	for (iterC = vKeyCharOriginal.begin(); iterC != vKeyCharOriginal.end(); ++iterC)
+	{
+		pos++;
+		cout << *iterC << " is at position " << pos << "." << endl;
+	}
+	cout << " is the orginal key in the vector vKeyCharOriginal." << endl;
+
+	sort(vSortedKey.begin(), vSortedKey.end());
+
+	for (iterC = vSortedKey.begin(); iterC != vSortedKey.end(); ++iterC)
+	{
+		cout << *iterC;
+	}
+	cout << " is the sorted key. " << endl;
+	return;
+
+	int i = 1;
+	for (iterC = vSortedKey.begin(); iterC != vSortedKey.end(); ++iterC)
+	{
+		vKeyInt.push_back(i);
+		i++;
+	}
+
+	KeyChar* pNewKeyChar = new KeyChar();
+
+	if (m_pHead == 0)
+	{
+		m_pHead = pNewKeyChar;
+		m_pLast = pNewKeyChar;
+	}
+	else
+	{
+		KeyChar* pIter = m_pHead;
+		m_pLast->SetNext(pNewKeyChar);
+		m_pLast = pNewKeyChar;
+	}
+}
+
+void Key::ClearKey()
+{
+	while (m_pHead != 0)
+	{
+		DelFirstKey();
+	}
+}
+
+void Key::DelFirstKey()
+{
+	if (m_pHead == 0)
+	{
+		cout << "There are no keys!" << endl;
+	}
+	else
+	{
+		KeyChar* pTemp = m_pHead;
+
+		m_pHead = m_pHead->GetNext();
+
+		delete pTemp;
+	}
+}
 
 int main()
 {
 	string key("SECRET");
-	vector<char> vKeyChar(key.begin(), key.end());
-	vector<int>vKeyInt;
-	vector<char>::const_iterator iterC;
-	vector<int>::const_iterator iterI;
-	int intGrid[6][6];
-	char charGrid[6][6];
-
-	sort(vKeyChar.begin(), vKeyChar.end());
-	int i = 1;
-	for (iterC = vKeyChar.begin(); iterC != vKeyChar.end(); ++iterC)
-	{
-		vKeyInt.push_back(i);
-		cout << *iterC;
-		i++;
-	}
-	cout << endl;
 	
-	for (iterI = vKeyInt.begin(); iterI != vKeyInt.end(); ++iterI)
-	{
-		cout << *iterI;
-	}
+	Key myKey;
+	Key& rMyKey = myKey;
 
-	cout << endl;
+	myKey.AddKeyChar(key); // sends key string to AddKeyChars
 
 	
+
+	cout << endl;
 
 	cin.get();
 
