79e91752b3080c55e093ce110a9d97459b7a9cd3
jcraggie <jcraggie@hotmail.com>
2016-09-27 12:59:00 -0500

    v3-3 cleaned up unneeded code. created functions for common routines used. could still do more.

--- Pockenacci_v3/Pockenacci_v3/Pockenacci_v3.cpp
+++ Pockenacci_v3/Pockenacci_v3/Pockenacci_v3.cpp
@@ -53,6 +53,94 @@ bool OriginalOrder(const CKeyChar& a, const CKeyChar& b)
 	return a.pos < b.pos;
 }
 
+void CopyGrid(char orig[6][6], char newarr[6][6])
+{
+	for (int r = 0; r < 6; ++r)
+	{
+		for (int c = 0; c < 6; ++c)
+		{
+			newarr[r][c] = orig[r][c];
+		}
+	}
+}
+
+void OutputGrid(char grid[6][6], string msg)
+{
+	cout << endl << msg << endl;
+	for (int r = 0; r < 6; ++r)
+	{
+		for (int c = 0; c < 6; ++c)
+			cout << grid[r][c] << " ";
+		cout << endl;
+	}
+}
+
+void OutputGrid(int grid[6][6], string msg)
+{
+	cout << endl << msg << endl;
+	for (int r = 0; r < 6; ++r)
+	{
+		for (int c = 0; c < 6; ++c)
+			cout << grid[r][c] << " ";
+		cout << endl;
+	}
+}
+
+void LookUpGrid(const char searchGrid[6][6], char findMe, int& fRow, int& fCol)
+{
+	for (int r = 0; r < 6; ++r)
+		for (int c = 0; c < 6; ++c)
+			if (searchGrid[r][c] == findMe)
+			{
+				fRow = r;
+				fCol = c;
+				//cout << "Found " << findMe << "at " << fRow << "," << fCol << endl;
+				return;
+			}
+}
+
+void ShiftGridDown(int cipher[6][6], char grid[6][6], int cipherRow)
+{
+	int shiftNum;
+	char tempChar;
+	for (int c = 0; c < 6; ++c) // shift each column down
+	{
+		shiftNum = cipher[cipherRow][c] % 6; // see table above - taking first row (row 0) of the cipherGrid for this shift
+		for (int i = 0; i < shiftNum; ++i) // shifting shiftNum times
+		{
+			tempChar = grid[5][c]; // hold last column item in temp. this will be moved to 0,c at the end
+			for (int r = 5; r >= 0; --r)
+			{
+				if (r - 1 < 0)
+					grid[r][c] = tempChar; // if at 0,c, get temp char and put here
+				else
+					grid[r][c] = grid[r - 1][c];
+			}
+		}
+	}
+}
+
+void ShiftGridRight(int cipher[6][6], char grid[6][6], int cipherRow)
+{
+	int shiftNum;
+	char tempChar;
+	for (int r = 0; r < 6; ++r) // shift each row right
+	{
+		shiftNum = cipher[cipherRow][r] % 6; // see table above - taking second row (row 1) of the cipherGrid for this shift
+		for (int i = 0; i < shiftNum; ++i) // shifting shiftNum times
+		{
+			tempChar = grid[r][5]; // hold last column item in temp. this will be moved to 0,c at the end
+			for (int c = 5; c >= 0; --c)
+			{
+				if (c - 1 < 0)
+					grid[r][c] = tempChar; // if at 0,c, get temp char and put here
+				else
+					grid[r][c] = grid[r][c - 1];
+			}
+		}
+	}
+}
+
 int main()
 {
 	string keyString("SECRET");
@@ -62,9 +150,6 @@ int main()
 	vector<CKeyChar> key;
 
 	int cipherGrid[6][6]{}; // 6x6 grid using int
-
-							//vector<vector<char>> cipherGrid(6, vector<char>(6)); // define 6x6 vector grid addressed as cipherGrid[r][c]
-							// std::string s = std::to_string(5); // use this to change an int to char;
 
 	int pos = 0;
 
@@ -111,34 +196,6 @@ int main()
 
 	int keySize = key.size();
 
-	//cout << key[0].c << " " << key[0].value << endl;
-
-
-
-
-
-	// end of output
-
-	// -------------------- MISTAKE BELOW
-	//iterK = key.begin();
-	//for (int r = 0; r < 6; ++r)
-	//{
-	//	for (int c = 0; c < 6; ++c)
-	//	{
-	//		if (iterK == key.end())
-	//		{
-	//			break;
-	//		}
-	//		else
-	//		{
-	//			cipherGrid[r][c] = iterK->value;
-	//			++iterK; // need to test for iterK = key.end()
-	//		}
-	//		
-	//	}
-	//}
-	// ---------------------------ABOVE IS MISTAKE. THE INITIAL KEY IS NOT PART OF THE 6X6 CIPHER
-
 	pos = 0;
 	int pos2;
 	int fibR, fibC, fibC2;
@@ -176,15 +233,8 @@ int main()
 
 		}
 	}
-
-
-	// output grid
-	for (int r = 0; r < 6; ++r)
-	{
-		for (int c = 0; c < 6; ++c)
-			cout << cipherGrid[r][c] << " ";
-		cout << endl;
-	}
+	
+	OutputGrid(cipherGrid, "This is the cipherGrid:");
 
 
 	// version 2 will be Loading Plaintext (A message to encrypt).
@@ -199,8 +249,6 @@ int main()
 
 	string plainText = "THIS IS A SECRET MESSAGE THAT WE NEED TO HIDE";
 
-	//char textGrid[6][6]{ " " };
-	//std::array<std::array<char, 6>, 6> textGrid{ " " };
 	char textGrid[6][6];
 	vector<char> vPlainText(plainText.begin(), plainText.end()); // puts individual chars into vector vKey
 																 // can user iterC from earlier
@@ -240,42 +288,15 @@ int main()
 
 
 	// output grid
-	cout << endl << "This is the plain text message to be encrypted: " << endl;
-	for (int r = 0; r < 6; ++r)
-	{
-		for (int c = 0; c < 6; ++c)
-			cout << textGrid[r][c] << " ";
-		cout << endl;
-	}
-
+	OutputGrid(textGrid, "This is the plain text message to be encrypted: ");
 
 	// version 3 - permutations
 	// we'll be shifting the columns down by the numbers in the first row of the key
 	// we'll then shift the rows by the numbers in the second row of the key
 
-
-
-
-	//char shiftDown1[6][6]{' '};
-
-	//model 2 dim array using std::array
-	//std::array<std::array<int, 3>, 3> arr = { { { 5, 8, 2 },{ 8, 3, 1 },{ 5, 3, 9 } } };
-
-	//std::array<std::array<char, 6>, 6> shiftDown1{ " " };
 	char shiftDown1[6][6]{};
 	
-	for (int r = 0; r < 6; ++r)
-	{
-		for (int c = 0; c < 6; ++c)
-		{
-			shiftDown1[r][c] = textGrid[r][c];
-		}
-	}
-	//shiftDown1 = textGrid;
-
-	// shifting columns down
-	// when shifting columns, col # stays the same; row # changes.
-	// if row # = 6, then next row = 0
+	CopyGrid(textGrid, shiftDown1);
 
 	int shiftNum = 0;
 	int currR = 0;
@@ -295,44 +316,16 @@ int main()
 	//     9          3
 	
 
-
-	for (int c = 0; c < 6; ++c) // shift each column down
-	{
-		shiftNum = cipherGrid[0][c] % 6; // see table above - taking first row (row 0) of the cipherGrid for this shift
-		for (int i = 0; i < shiftNum; ++i) // shifting shiftNum times
-		{
-			tempChar = shiftDown1[5][c]; // hold last column item in temp. this will be moved to 0,c at the end
-			for (int r = 5; r >= 0; --r)
-			{
-				if (r - 1 < 0)
-					shiftDown1[r][c] = tempChar; // if at 0,c, get temp char and put here
-				else
-					shiftDown1[r][c] = shiftDown1[r - 1][c];
-			}
-		}
-	}
-
+	ShiftGridDown(cipherGrid, shiftDown1, 0);
 
 	// output grid
-	cout << endl << "This is the shiftDown1 grid after shifting each column: " << endl;
-	for (int r = 0; r < 6; ++r)
-	{
-		for (int c = 0; c < 6; ++c)
-			cout << shiftDown1[r][c] << " ";
-		cout << endl;
-	}
+	OutputGrid(shiftDown1, "This is the shiftDown1 grid after shifting each column: ");
+
 
 	// create new grid for shiftRight1 based on shiftDown1
 	char shiftRight1[6][6]{};
 
-	for (int r = 0; r < 6; ++r)
-	{
-		for (int c = 0; c < 6; ++c)
-		{
-			shiftRight1[r][c] = shiftDown1[r][c];
-		}
-	}
-
+	CopyGrid(shiftDown1, shiftRight1);
 
 
 	// do a table if shift # is 6, 7, 8, or 9
@@ -343,31 +336,13 @@ int main()
 	//     8          2
 	//     9          3
 	//
+
 	// now need to shift each row right by num of chars in 2nd row (row 1) of the cipherGrid
-	for (int r = 0; r < 6; ++r) // shift each row right
-	{
-		shiftNum = cipherGrid[1][r] % 6; // see table above - taking second row (row 1) of the cipherGrid for this shift
-		for (int i = 0; i < shiftNum; ++i) // shifting shiftNum times
-		{
-			tempChar = shiftRight1[r][5]; // hold last column item in temp. this will be moved to 0,c at the end
-			for (int c = 5; c >= 0; --c)
-			{
-				if (c - 1 < 0)
-					shiftRight1[r][c] = tempChar; // if at 0,c, get temp char and put here
-				else
-					shiftRight1[r][c] = shiftRight1[r][c - 1];
-			}
-		}
-	}
+	ShiftGridRight(cipherGrid, shiftRight1, 1);
 
 	// output grid
-	cout << endl << "This is the shiftRight1 grid after shifting each column: " << endl;
-	for (int r = 0; r < 6; ++r)
-	{
-		for (int c = 0; c < 6; ++c)
-			cout << shiftRight1[r][c] << " ";
-		cout << endl;
-	}
+	OutputGrid(shiftRight1, "This is the shiftRight1 grid after shifting each column: ");
+
 
 	// version 3-2 - substitution (Let's change things completely).
 	// now that we have shifted each column and each row, we cant to actually change their value.
@@ -387,55 +362,16 @@ int main()
 		'0','1','2','3','4','5','6','7','8','9' };
 
 	cout << endl;
-	// testing s-box lookup -------------------------------------------------
-	for (int i = 0; i < 36; ++i)
-	{
-		cout << setw(3) << sBox[i];
-	}
-	cout << endl;
-
-	for (int i = 0; i < 36; ++i)
-	{
-		cout << setw(3) << i;
-	}
-	cout << endl;
-
-	char chars[4]{ 0 }, cNew;
-	int idx, newIdx, j;
-
-	chars[0] = 'W';
-	chars[1] = 'Z';
-	chars[2] = '8';
-	chars[3] = 'K';
-	for (j = 0; j < 4; ++j)
-	{
-		for (int i = 0; i < 36; ++i)
-			if (sBox[i] == chars[j])
-			{
-				idx = i;
-				newIdx = (idx + 7) % 36;
-
-				cNew = sBox[newIdx];
-			}
-
-		cout << "Char c1: " << chars[j] << " is located at " << idx << " and 7 chars from that is " << cNew << " located at " << newIdx << endl;
-	}
-	// end s-box testing -----------------------------------------------------
 
 	// create sBox grid for lookups
 	// create new grid for shiftRight1 based on shiftDown1
 	char sBoxGrid[6][6]{};
 
-	for (int r = 0; r < 6; ++r)
-	{
-		for (int c = 0; c < 6; ++c)
-		{
-			sBoxGrid[r][c] = shiftRight1[r][c];
-		}
-	}
+	CopyGrid(shiftRight1, sBoxGrid);
 	
-
+	
 	// now need to shift each row right by num of chars in 2nd row (row 1) of the cipherGrid
+	int idx, newIdx;
 	for (int r = 0; r < 6; ++r) // iterate through each row
 	{
 		for (int c = 0; c < 6; ++c)
@@ -458,15 +394,7 @@ int main()
 		
 	}
 
-
-	// output grid
-	cout << endl << "This is sBoxGrid after looking up each row in the sBox array: " << endl;
-	for (int r = 0; r < 6; ++r)
-	{
-		for (int c = 0; c < 6; ++c)
-			cout << sBoxGrid[r][c] << " ";
-		cout << endl;
-	}
+	OutputGrid(sBoxGrid, "This is sBoxGrid after looking up each row in the sBox array: ");
 
 
 	// version 3-3 Mesasge Authentication Codes (Check yourself!) MAC
@@ -476,6 +404,7 @@ int main()
 	// creates the abcGrid used to look up values from the sBoxGrid also using the cipherGrid
 	char abcGrid[6][6]{};
 	int sBoxi = 0; // index used to iterate through the sBox array and put it in the abcGrid
+
 	for (int r = 0; r < 6; ++r)
 	{
 		for (int c = 0; c < 6; ++c)
@@ -485,27 +414,28 @@ int main()
 		}
 	}
 
+	OutputGrid(abcGrid, "This is abcGrid:");
+
 	// lookup each value from sBoxGrid in the abcGrid table. Need to capture the ROW and COLUMN when found
+	int macGrid[6][6]{};
+	char findMe;
+	char macChar;
+	int macInt;
+	int fRow, fCol;
 	for (int r = 0; r < 6; ++r)
 	{
 		for (int c = 0; c < 6; ++c)
 		{
+			findMe = sBoxGrid[r][c];
+			LookUpGrid(abcGrid, findMe, fRow, fCol);
+			macInt = cipherGrid[fRow][fCol];
+			//cout << "macInt" << macInt << endl;
+			macGrid[r][c] = macInt;
 			
 		}
 	}
 
-	
-
-	// output grid
-	cout << endl << "This is sBox grid abcGrid: " << endl;
-	for (int r = 0; r < 6; ++r)
-	{
-		for (int c = 0; c < 6; ++c)
-			cout << abcGrid[r][c] << " ";
-		cout << endl;
-	}
-
-
+	OutputGrid(macGrid, "This is first phase of the MAC:");
 
 	return 0;
 }
