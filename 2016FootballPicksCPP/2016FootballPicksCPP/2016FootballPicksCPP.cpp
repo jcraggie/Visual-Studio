9dabffa45a4fbeec88d969cc82719a56059bbb5f
Jason C. Rogers <jcraggie@hotmail.com>
2016-10-02 21:07:44 -0500

    formatted most input and output. have test output installed.

--- 2016FootballPicksCPP/2016FootballPicksCPP/2016FootballPicksCPP.cpp
+++ 2016FootballPicksCPP/2016FootballPicksCPP/2016FootballPicksCPP.cpp
@@ -2,6 +2,7 @@
 //
 
 #include "stdafx.h"
+#include "FootballPicks.h"
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -11,34 +12,49 @@
 
 using namespace std;
 
-class Game
+void RemoveRanking(string& word)
 {
-private:
-	string m_Away;
-	string m_Home;
-	string m_UD;
-	int m_UDpts;
-	string m_Date;
-	string m_Time;
-	string m_AwayConf;
-	string m_HomeConf;
-	int m_AwayScore;
-	int m_HomeScore;
-	string m_WinnerGame;
-	string m_WinnerSpread;
-	
-};
+	string temp=" ";
+	int start = 0;
+	int size = 0;
+	int j = 0;
+	if (word[0] != '(')
+		return;
+	size = word.size();
+	for (int i = 0; i < size; ++i)
+	{
+		if (word[i] == ')')
+		{
+			start = i + 2;
+			break;
+		}
+	}
+	word.erase(0, start);
+	//for (int i = start; i < size; ++i)
+	//{
+	//	temp[j] = word[i];
+	//	j += 1;
+	//}
+
+	//word = temp;
+	return;
+}
+
 
 int main()
 {
 	vector<string> line;
 	string str;
 	string w;
-	string game;
+	string gameLine;
 	//vector<char> parseLine;
 	vector<string>::iterator iterC;
 	string word;
 	vector<string> gameInfo;
+	int wordCount = 0;
+	int amt = 0;
+	vector<CGame> game;
+	vector<CGame>::iterator iterG;
 
 
 	//ofstream myFile("2016gameData1.txt");
@@ -50,40 +66,176 @@ int main()
 	//}
 	//else cout << "Unable to open file to write." << endl;
 
-	ifstream myFile2 ("2016gameData1.txt");
+	ifstream myFile2("2016 Football Picks4-1.txt");
+	//ifstream myFile2 ("2016gameData1.txt");
 	while (myFile2)
 	{
 		getline(myFile2,str);
 		line.push_back(str);
 
-		cout << str << endl;
+		//cout << str << endl;
 	}
 
 	for (iterC = line.begin(); iterC != line.end(); ++iterC)
 	{
-		game = *iterC; // 1 word
+		CGame* newGame = new CGame;
+		gameLine = *iterC; // 1 word
+		wordCount = 0;
 		//cout << "game size is: " << game.size() << endl;
 		//cout << "Word is: " << w << endl;
-		for (int i = 0; i < game.size(); ++i)
+		for (int i = 0; i < gameLine.size(); ++i) // loop through entire game line
 		{
-			if (game[i] != '\t') // if the char is not a tab
+			if (gameLine[i] != '\t') // if the char is not a tab
 			{
-				word.push_back(game[i]); // push the char found to word. once encounter tab, word is done.
+				word.push_back(gameLine[i]); // push the char found to word. once encounter tab, word is done.
 			}
 			else
 			{
-				cout << setw(15) << word;
+				wordCount += 1;
+				//cout << setw(20) << word;
+				
+				switch (wordCount)
+				{
+				case 1:
+					newGame->SetTime(word);
+					break;
+				case 2:
+					newGame->SetAwayTeam(word);
+					break;
+				case 3:
+					newGame->SetHomeTeam(word);
+					break;
+				case 4:
+					RemoveRanking(word);
+					newGame->SetUDTeam(word);
+					break;
+				case 5:
+					amt = std::stoi(word);
+					newGame->SetUDPts(amt);
+					break;
+				case 6:
+					newGame->SetAwayRank(word);
+					break;
+				case 7:
+					newGame->SetHomeRank(word);
+					break;
+				case 8:
+					newGame->SetUDrank(word);
+					break;
+				case 9:
+					newGame->SetAwayConf(word);
+					break;
+				case 10:
+					newGame->SetHomeConf(word);
+					break;
+				case 11:
+					newGame->SetDate(word);
+					break;
+				case 12:
+					amt = std::stoi(word);
+					newGame->SetAwayScore(amt);
+					break;
+				case 13:
+					amt = std::stoi(word);
+					newGame->SetHomeScore(amt);
+					break;
+				case 14:
+					RemoveRanking(word);
+					newGame->SetJMCgamePick(word);
+					break;
+				case 15:
+					RemoveRanking(word);
+					newGame->SetJCRgamePick(word);
+					break;
+				case 16:
+					RemoveRanking(word);
+					newGame->SetJMCspreadPick(word);
+					break;
+				case 17:
+					RemoveRanking(word);
+					newGame->SetJCRspreadPick(word);
+					break;
+				case 18:
+					RemoveRanking(word);
+					newGame->SetTeamGameWinner(word);
+					break;
+				case 19:
+					RemoveRanking(word);
+					newGame->SetTeamSpreadWinner(word);
+					break;
+				case 20:
+					amt = std::stoi(word);
+					newGame->SetJMCwinGame(amt);
+					break;
+				case 21:
+					amt = std::stoi(word);
+					newGame->SetJCRwinGame(amt);
+					break;
+				case 22:
+					amt = std::stoi(word);
+					newGame->SetJMCwinSpread(amt);
+					break;
+				case 23:
+					amt = std::stoi(word);
+					newGame->SetJCRwinSpread(amt);
+					break;
+				case 24:
+					newGame->SetGameType(word);
+					break;
+				}
 				word = "";
 			}
 
 			
 		}
-		cout << setw(15) << word;
+		game.push_back(*newGame);
+		//cout << setw(15) << word;
 		word = "";
-		cout << endl;
+		//cout << endl;
 		
 	}
-
+	std::string awayRankAndTeam;
+	std::string homeRankAndTeam;
+	cout << endl << endl << endl << "Looping through CGame class variable game: " << endl << endl;
+	for (iterG = game.begin(); iterG != game.end(); ++iterG)
+	{
+		cout << "--------------------------------------------------------------------------------------------------------------------------------------------------" << endl;
+		if (iterG->GetAwayRank() != "")
+			awayRankAndTeam = "#" + iterG->GetAwayRank() + " " + iterG->GetAwayTeam();
+		else
+			awayRankAndTeam = iterG->GetAwayRank() + " " + iterG->GetAwayTeam();
+		if (iterG->GetHomeRank() != "")
+			homeRankAndTeam = "#" + iterG->GetHomeRank() + " " + iterG->GetHomeTeam();
+		else
+			homeRankAndTeam = iterG->GetHomeRank() + " " + iterG->GetHomeTeam();
+		cout << right << setw(9) << iterG->GetTime();
+		cout << right << setw(7) << iterG->GetAwayConf();
+		//cout << setw(3) << iterG->GetAwayRank();
+		cout << right << setw(20) << awayRankAndTeam;
+		if (iterG->GetUDTeam() == iterG->GetAwayTeam())
+			cout << right << setw(2) << "+" << left << setw(3) << abs(iterG->GetUDPts()) << right;
+		else
+			cout << right << setw(5) << " ";
+		//cout << right << setw(7) << iterG->GetAwayConf();
+		cout << setw(3) << iterG->GetAwayScore();
+		cout << setw(20) << iterG->GetTeamGameWinner();
+		cout << setw(20) << iterG->GetJMCgamePick();
+		cout << setw(20) << iterG->GetJCRgamePick();
+		cout << setw(20) << iterG->GetJMCspreadPick();
+		cout << setw(20) << iterG->GetJCRspreadPick();
+		cout << endl;
+		//cout << setw(12) << iterG->GetHomeRank();
+		cout << right << setw(16) << iterG->GetHomeConf();
+		cout << right << setw(20) << homeRankAndTeam;
+		if (iterG->GetUDTeam() == iterG->GetHomeTeam())
+			cout << right << setw(2) << "+" << left << setw(3) << abs(iterG->GetUDPts()) << right;
+		else
+			cout << right << setw(5) << " ";
+		//cout << right << setw(7) << iterG->GetHomeConf();
+		cout << setw(3) << iterG->GetHomeScore();
+		cout << setw(20) << iterG->GetTeamSpreadWinner();
+		cout << endl;
+	}
 
 
     return 0;
---
+++ 2016FootballPicksCPP/2016FootballPicksCPP/FootballPicks.h
@@ -0,0 +1,335 @@
+#pragma once
+
+#include <iostream>
+
+class CGame
+{
+public:
+	CGame();
+
+	// getters
+	std::string GetTime() const;
+	std::string GetAwayTeam() const;
+	std::string GetHomeTeam() const;
+	std::string GetUDTeam() const;
+	int GetUDPts() const;
+	std::string GetAwayRank() const;
+	std::string GetHomeRank() const;
+	std::string GetUDrank() const;
+	std::string GetAwayConf() const;
+	std::string GetHomeConf() const;
+	std::string GetDate() const;
+	int GetAwayScore() const;
+	int GetHomeScore() const;
+	std::string GetJMCgamePick() const;
+	std::string GetJCRgamePick() const;
+	std::string GetJMCspreadPick() const;
+	std::string GetJCRspreadPick() const;
+	std::string GetTeamGameWinner() const;
+	std::string GetTeamSpreadWinner() const;
+	bool GetJMCwinGame() const;
+	bool GetJCRwinGame() const;
+	bool GetJMCwinSpread() const;
+	bool GetJCRwinSpread() const;
+	std::string GetGameType() const;
+
+	// setters
+	void SetTime(std::string time);
+	void SetAwayTeam(std::string awayTeam);
+	void SetHomeTeam(std::string homeTeam);
+	void SetUDTeam(std::string UDteam);
+	void SetUDPts(int UDpts);
+	void SetAwayRank(std::string awayRank);
+	void SetHomeRank(std::string homeRank);
+	void SetUDrank(std::string UDrank);
+	void SetAwayConf(std::string awayConf);
+	void SetHomeConf(std::string homeConf);
+	void SetDate(std::string date);
+	void SetAwayScore(int awayScore);
+	void SetHomeScore(int homeScore);
+	void SetJMCgamePick(std::string JMCgamePick);
+	void SetJCRgamePick(std::string JCRgamePick);
+	void SetJMCspreadPick(std::string JMCspreadPick);
+	void SetJCRspreadPick(std::string JCRspreadPick);
+	void SetTeamGameWinner(std::string gameWinner);
+	void SetTeamSpreadWinner(std::string spreadWinner);
+	void SetJMCwinGame(bool JMCwinGame);
+	void SetJCRwinGame(bool JCRwinGame);
+	void SetJMCwinSpread(bool JMCwinSpread);
+	void SetJCRwinSpread(bool JCRwinSpread);
+	void SetGameType(std::string gameType);
+private:
+	std::string m_Time;
+	std::string m_AwayTeam;
+	std::string m_HomeTeam;
+	std::string m_UDTeam;
+	int m_UDpts;
+	std::string m_AwayRank;
+	std::string m_HomeRank;
+	std::string m_UDrank;
+	std::string m_AwayConf;
+	std::string m_HomeConf;
+	std::string m_Date;
+	int m_AwayScore;
+	int m_HomeScore;
+	std::string m_JMCgamePick;
+	std::string m_JCRgamePick;
+	std::string m_JMCspreadPick;
+	std::string m_JCRspreadPick;
+	std::string m_WinnerGame;
+	std::string m_WinnerSpread;
+	bool m_JMCwinGame;
+	bool m_JCRwinGame;
+	bool m_JMCwinSpread;
+	bool m_JCRwinSpread;
+	std::string m_GameType; // CFB or NFL
+};
+
+CGame::CGame()
+{
+
+}
+
+std::string CGame::GetTime() const
+{
+	return m_Time;
+}
+
+std::string CGame::GetAwayTeam() const
+{
+	return m_AwayTeam;
+}
+
+std::string CGame::GetHomeTeam() const
+{
+	return m_HomeTeam;
+}
+
+std::string CGame::GetUDTeam() const
+{
+	return m_UDTeam;
+}
+
+int CGame::GetUDPts() const
+{
+	return m_UDpts;
+}
+
+std::string CGame::GetAwayRank() const
+{
+	return m_AwayRank;
+}
+
+std::string CGame::GetHomeRank() const
+{
+	return m_HomeRank;
+}
+
+std::string CGame::GetUDrank() const
+{
+	return m_UDrank;
+}
+
+std::string CGame::GetAwayConf() const
+{
+	return m_AwayConf;
+}
+
+std::string CGame::GetHomeConf() const
+{
+	return m_HomeConf;
+}
+
+std::string CGame::GetDate() const
+{
+	return m_Date;
+}
+
+int CGame::GetAwayScore() const
+{
+	return m_AwayScore;
+}
+
+int CGame::GetHomeScore() const
+{
+	return m_HomeScore;
+}
+
+std::string CGame::GetJMCgamePick() const
+{
+	return m_JMCgamePick;
+}
+
+std::string CGame::GetJCRgamePick() const
+{
+	return m_JCRgamePick;
+}
+
+std::string CGame::GetJMCspreadPick() const
+{
+	return m_JMCspreadPick;
+}
+
+std::string CGame::GetJCRspreadPick() const
+{
+	return m_JCRspreadPick;
+}
+
+std::string CGame::GetTeamGameWinner() const
+{
+	return m_WinnerGame;
+}
+
+std::string CGame::GetTeamSpreadWinner() const
+{
+	return m_WinnerSpread;
+}
+
+bool CGame::GetJMCwinGame() const
+{
+	return m_JMCwinGame;
+}
+
+bool CGame::GetJCRwinGame() const
+{
+	return m_JCRwinGame;
+}
+
+bool CGame::GetJMCwinSpread() const
+{
+	return m_JMCwinSpread;
+}
+
+bool CGame::GetJCRwinSpread() const
+{
+	return m_JCRwinSpread;
+}
+
+std::string CGame::GetGameType() const
+{
+	return m_GameType;
+}
+
+
+
+void CGame::SetTime(std::string time)
+{
+	m_Time = time;
+}
+
+void CGame::SetAwayTeam(std::string awayTeam)
+{
+	m_AwayTeam = awayTeam;
+}
+
+void CGame::SetHomeTeam(std::string homeTeam)
+{
+	m_HomeTeam = homeTeam;
+}
+
+void CGame::SetUDTeam(std::string UDteam)
+{
+	m_UDTeam = UDteam;
+}
+
+void CGame::SetUDPts(int UDpts)
+{
+	m_UDpts = UDpts;
+}
+
+void CGame::SetAwayRank(std::string awayRank)
+{
+	m_AwayRank = awayRank;
+}
+
+void CGame::SetHomeRank(std::string homeRank)
+{
+	m_HomeRank = homeRank;
+}
+
+void CGame::SetUDrank(std::string UDrank)
+{
+	m_UDrank = UDrank;
+}
+
+void CGame::SetAwayConf(std::string awayConf)
+{
+	m_AwayConf = awayConf;
+}
+
+void CGame::SetHomeConf(std::string homeConf)
+{
+	m_HomeConf = homeConf;
+}
+
+void CGame::SetDate(std::string date)
+{
+	m_Date = date;
+}
+
+void CGame::SetAwayScore(int awayScore)
+{
+	m_AwayScore = awayScore;
+}
+
+void CGame::SetHomeScore(int homeScore)
+{
+	m_HomeScore = homeScore;
+}
+
+void CGame::SetJMCgamePick(std::string JMCgamePick)
+{
+	m_JMCgamePick = JMCgamePick;
+}
+
+void CGame::SetJCRgamePick(std::string JCRgamePick)
+{
+	m_JCRgamePick = JCRgamePick;
+}
+
+void CGame::SetJMCspreadPick(std::string JMCspreadPick)
+{
+	m_JMCspreadPick = JMCspreadPick;
+}
+
+void CGame::SetJCRspreadPick(std::string JCRspreadPick)
+{
+	m_JCRspreadPick = JCRspreadPick;
+}
+
+void CGame::SetTeamGameWinner(std::string teamGameWinner)
+{
+	m_WinnerGame = teamGameWinner;
+}
+
+void CGame::SetTeamSpreadWinner(std::string teamSpreadWinner)
+{
+	m_WinnerSpread = teamSpreadWinner;
+}
+
+void CGame::SetJMCwinGame(bool JMCwinGame)
+{
+	m_JMCwinGame = JMCwinGame;
+}
+
+void CGame::SetJCRwinGame(bool JCRwinGame)
+{
+	m_JCRwinGame = JCRwinGame;
+}
+
+void CGame::SetJMCwinSpread(bool JMCwinSpread)
+{
+	m_JMCwinSpread = JMCwinSpread;
+}
+
+void CGame::SetJCRwinSpread(bool JCRwinSpread)
+{
+	m_JCRwinSpread = JCRwinSpread;
+}
+
+void CGame::SetGameType(std::string gameType)
+{
+	m_GameType = gameType;
+}
+
+
