48b4151242731a12abf54552e0be120b90e35585
Jason C. Rogers <jcraggie@hotmail.com>
2016-10-14 23:34:50 -0500

    Version 5 - cleaner code using virtual functions and override functions.

--- CSengineersCh6/CSengineersCh6/CodeFile1.cs
+++ CSengineersCh6/CSengineersCh6/CodeFile1.cs
@@ -1,11 +1,5 @@
 ﻿using System;
 
-enum EngineerTypeEnum
-{
-    Engineer,
-    CivilEngineer
-}
-
 class Engineer
 {
     //constructor
@@ -13,54 +7,35 @@ class Engineer
     {
         this.name = name;
         this.billingRate = billingRate;
-        type = EngineerTypeEnum.Engineer;
     }
 
     //figure out the charge based on the engineer's rate
-    public float CalculateCharge(float hours)
+    //now is virtual
+    virtual public float CalculateCharge(float hours)
     {
-        if (type == EngineerTypeEnum.CivilEngineer)
-        {
-            CivilEngineer c = (CivilEngineer)this;
-            return (c.CalculateCharge(hours));
-        }
-        else if (type == EngineerTypeEnum.Engineer)
-        {
-            return (hours * billingRate);
-        }
-        return (0F);
+        return (hours * billingRate);
     }
 
     //return the name of this type
-    public string TypeName()
+    //now is virtual
+    virtual public string TypeName()
     {
-        if (type == EngineerTypeEnum.CivilEngineer)
-        {
-            CivilEngineer c = (CivilEngineer)this;
-            return (c.TypeName());
-        }
-        else if (type == EngineerTypeEnum.Engineer)
-        {
-            return ("Engineer");
-        }
-        return ("No Type Matched");
+        return ("Engineer");
     }
 
     private string name;
     protected float billingRate;
-    protected EngineerTypeEnum type;
 }
 
 class CivilEngineer : Engineer
 {
     public CivilEngineer(string name, float billingRate) :
         base(name, billingRate)
-    {
-        type = EngineerTypeEnum.CivilEngineer;
-    }
+    { }
 
     //new function because it is different than the base version
-    public new float CalculateCharge(float hours)
+    //overrides function in Engineer
+    override public float CalculateCharge(float hours)
     {
         if (hours < 1.0F)
             hours = 1.0F; //minimum charge
@@ -68,7 +43,8 @@ class CivilEngineer : Engineer
     }
 
     //new function becuase it is different than the base version
-    public new string TypeName()
+    //overrides function in Engineer
+    override public string TypeName()
     {
         return ("Civil Engineer");
     }
